68d0e409ca2645452b7a1d41ebbadfcaf1edf7e3
En-Wei Yeh <wei.yeh@rwth-aachen.de>
2025-05-11 15:11:14 +0200

    Basic example

---
+++ my_module/main.py
@@ -0,0 +1,9 @@
+# my_module/main.py
+
+def main():
+    print("Hello from pyproject-based module!")
+    value = 123  # Set breakpoint here
+    print(value)
+
+if __name__ == "__main__":
+    main()
